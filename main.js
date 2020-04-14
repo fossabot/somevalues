c5ebec6af28c66c86973dae407f06492b78147bf
Aditya <taplejung@protonmail.ch>
2020-04-14 12:19:06 +0100

    Fixed writePhrase and other localisation fixes

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
-var localisation = null;
+var localisationCache = null;
+var language = null;
 
 function getLocalisation(lang) {
-	fetch("localisation/" + lang + ".json")
-		.then(response => response.json())
-		.then(data =>  localisation = data);
+	if (localisationCache == null) {
+		return fetch("localisation/" + lang + ".json")
+			.then(response => response.json())
+			.then(data => localisationCache = data);
+	} else {
+		return localisationCache;
+	}
 }
 
-function writePhrase(phraseid) {
-	document.currentScript.insertAdjacentHTML("beforebegin", localisation[phraseid]);
+async function writePhrase(phraseid) {
+	var pidParts = phraseid.split(".");
+	var localisationVal = await getLocalisation(language);
+	while (pidParts.length) {
+		localisationVal = localisationVal[pidParts.shift()];
+	}
+	document.currentScript.insertAdjacentHTML("beforebegin", localisationVal);
 }
 
-if (document.cookie.split(";").some((item) => item.trim().startsWith("lang="))) {
-	getLocalisation(document.cookie.split(";").find((item) => item.trim().startsWith("lang=")).substring(5));
+if (document.cookie.split(";").some(item => item.trim().startsWith("lang="))) {
+	language = document.cookie.split(";").find(item => item.trim().startsWith("lang=")).substring(5);
+} else {
+	language = "en";
 }
+getLocalisation(language);
